9a6351b2b351ebdc9a421f624aaf8e996f82c8ba
Ilya Iurkov <ilyaiurkov@gmail.com>
2026-07-22 23:48:53 +0200

    deploy prep

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,25 +1,26 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/gift/',
   plugins: [
     tanstackRouter({
-      target: "react",
-      routesDirectory: "src/app",
-      routeToken: "layout",
-      generatedRouteTree: "./src/routeTree.gen.ts",
+      target: 'react',
+      routesDirectory: 'src/app',
+      routeToken: 'layout',
+      generatedRouteTree: './src/routeTree.gen.ts',
     }),
     react(),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
